7d0d3aba90959d1463ead51e64205d2ee04c8f5f
Gabriel Kaue <gabrielkauerodriguest@gmail.com>
2025-08-12 20:36:57 -0300

    feat: exericio1

---
+++ src/main/java/cap1/exercicio1/Main.java
@@ -0,0 +1,8 @@
+package cap1.exercicio1;
+
+public class Main {
+    public static void main(String[] args) {
+       System.out.println("I rule!");
+       System.out.println("The World");
+    }
+}
